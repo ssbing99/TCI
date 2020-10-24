f08676f96e00dfd40b2a1621e00d11ec48afc233
Soon Lai <soonlai814@gmail.com>
2020-10-25 06:35:53 +0800

    update lang

--- resources/lang/en/custom-menu.php
+++ resources/lang/en/custom-menu.php
@@ -1,12 +1,11 @@
 <?php return array (
-  'nav-menu' => 
-  array (
-    'article-videos' => 'Article & Videos',
-    'courses-events' => 'Courses & Events',
-    'forums' => 'Forums',
-    'contact' => 'Contact',
-    'about-us' => 'About Us',
-    'long-term-programs' => 'Long Term Programs',
-      'courses' => 'Courses'
-  ),
+    'nav-menu' =>
+        array (
+            'article-videos' => 'Article & Videos',
+            'courses-events' => 'Courses & Events',
+            'long-term-programs' => 'Long Term Programs',
+            'contact' => 'Contact',
+            'about-us' => 'About Us',
+            'courses' => 'Courses',
+        ),
 );
