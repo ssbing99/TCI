f5f27cb6cde423a641c1114549fc10d56fcc12d8
Issac Tan <tswei6h@hotmail.com>
2021-05-11 22:40:36 +0800

    spacing and fix content

--- resources/views/emails/instructorCourseSignupMail.blade.php
+++ resources/views/emails/instructorCourseSignupMail.blade.php
@@ -1,8 +1,8 @@
 @component('mail::message')
 # Hello, {{ $content['receiver_name'] }}
 
-A student has just enrolled on your {{$course->title}} course. Please go to your Instructor Area and send them a brief “Welcome to the Course” message placed in the Discussion field at the bottom of the first Lesson.  
-Thanks!
-Regards,
+A student has just enrolled on your {{ $content['title'] }} course. Please go to your Instructor Area and send them a brief “Welcome to the Course” message placed in the Discussion field at the bottom of the first Lesson.  <br/><br/>
+Thanks!<br/><br/>
+Regards,<br/><br/>
 {{ config('app.name') }}
 @endcomponent
